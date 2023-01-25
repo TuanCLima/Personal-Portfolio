3e32bf89c8e84eccfe785198c9a767221c876bad
TuanCLima <tuanclima@gmail.com>
2023-01-24 21:53:45 -0300

    NavBar up and running (without links)

---
+++ src/app/components/navbar/NavBar.jsx
@@ -0,0 +1,28 @@
+
+import Link from 'next/link'
+import styles from './NavBar.module.css'
+import { useRef } from 'react'
+
+function NavBar() {
+  const listRef = useRef(null);
+
+  const handleClick = (event) => {
+    event.target.parentElement.childNodes.forEach((childNode) => {
+      childNode.classList.remove(styles.selected);
+    })
+    event.target.classList.add(styles.selected);
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles.list} ref={listRef} onClick={handleClick}>
+        <li className={styles.listItem}><Link className={styles.link} href="#">Home</Link></li>
+        <li className={styles.listItem}><Link className={styles.link} href="#">Experience</Link></li>
+        <li className={styles.listItem}><Link className={styles.link} href="#">Academia</Link></li>
+        <li className={`${styles.listItem} ${styles.selected}`}><Link className={styles.link} href="#">About</Link></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar;
