7ab66e38b4f5261ec8bdca976fee5fbb1a01373b
Guilherme Lima <gui7play@gmail.com>
2023-02-01 20:27:31 -0300

    refactor carousel, add radio buttons and open-close functionality

--- src/app/components/experience/Experience.jsx
+++ src/app/components/experience/Experience.jsx
@@ -5,113 +5,105 @@ import { useState, useCallback } from "react";
 
 function Experience() {
   const [expandedId, setExpandedId] = useState("");
-
-  const jobData = {
-    vix: {
-      title: "Vix",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      images: [
-        "/carrousel/cellphoneOne.png",
-        "/carrousel/desktopOne.png",
-        "/carrousel/cellphoneTwo.png",
-        "/carrousel/desktopTwo.png"
-      ]
-    },
-    granito: {
-      title: "Granito",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      images: [
-        "/carrousel/cellphoneOne.png",
-        "/carrousel/desktopOne.png",
-        "/carrousel/cellphoneTwo.png",
-        "/carrousel/desktopTwo.png"
-      ]
-    },
-    ateme: {
-      title: "Ateme",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      images: [
-        "/carrousel/cellphoneOne.png",
-        "/carrousel/desktopOne.png",
-        "/carrousel/cellphoneTwo.png",
-        "/carrousel/desktopTwo.png"
-      ]
-    }
-  };
-
-  let carouselIndex = 0;
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   /* not finished */
   const handleDetails = (e) => {
-    const detailsEl = document.getElementsByClassName(styles.jobDetails)[0];  
+    const detailsEl = document.getElementsByClassName(styles.jobDetails)[0]; 
 
     if(expandedId !== e.target.id) {
+      detailsEl.style.transform = "scaleY(1)";
+      detailsEl.style.height = "450px";
       setExpandedId(e.target.id);
       return;
     }
 
     if(expandedId === e.target.id) {
+      detailsEl.style.transform = "scaleY(0)";
+      detailsEl.style.height = "0px";
       setExpandedId("");
       return;
     }
   }
 
-  function moveCarousel(direction) { 
-    const carousel = document.getElementsByClassName(styles.carousel)[0];
-    const elements = document.getElementsByClassName(styles.element);
+  const calculateDistance = (destinyIndex) => {
+    const carouselChildren = document.getElementsByClassName(styles.carousel)[0].childNodes;
+    const convertedChildren = [...carouselChildren];
 
-    const convertedElements = [...elements];
-
-    const elementsWidth = convertedElements.map(element => {
+    const childrenWidth = convertedChildren.map(element => {
       const elementStyle = getComputedStyle(element);
       const numericWidth = parseInt(elementStyle.width.slice(0, -2));
       return numericWidth;
     });
-
-    const distanceFoward = (elementsWidth[carouselIndex] + (elementsWidth[carouselIndex + 1])) / 2;
-    const distanceBackward = (elementsWidth[carouselIndex] + (elementsWidth[carouselIndex - 1])) / 2;
-
-    const style = getComputedStyle(carousel);
-
-    /* 12px -> 12 */
-    const leftDistance = style.left.slice(0, -2);
-
-    const numericDistance = parseInt(leftDistance);
-
-    const limit = carousel.childNodes.length;
     
-    const canGoForward = ((numericDistance - distanceFoward) / distanceFoward) > -limit;
-    const canGoBackwards = ((numericDistance + distanceBackward) / distanceBackward <= 0);
-  
-  
-    if(direction === "forward" && canGoForward) {
-      carouselIndex += 1;
-      carousel.style.left = (numericDistance - distanceFoward) + "px";
-
-      convertedElements.forEach((element, index) => {
-        element.classList.add(styles.unfocused);
-        if(carouselIndex === index) {
-          element.classList.remove(styles.unfocused);
-        }
-      });
-      return;
+    let distance = 0;
+    
+    for(let i=currentIndex; i<=destinyIndex; i++) {
+      distance = distance + (childrenWidth[i] / 2);
     }
 
-    if(direction === "backward" && canGoBackwards){
-      carouselIndex -= 1;
-      carousel.style.left = (numericDistance + distanceBackward) + "px";
+    moveCarousel(destinyIndex, distance);
+  }
 
-      convertedElements.forEach((element, index) => {
+  const toggleRadio = (e) => {
+    const toggleList = document.getElementsByClassName(styles.radio);
+
+    const convertedList = [...toggleList];
+
+    convertedList.forEach(toggle => {
+      toggle.classList.remove(styles.selected);
+      if(toggle === e.target) {
+        toggle.classList.add(styles.selected);
+      }
+    });
+  }
+
+  const moveCarousel = (destinyIndex, distance) => {
+    const carousel = document.getElementsByClassName(styles.carousel)[0];
+    const style = getComputedStyle(carousel);
+    const currentDistance = parseInt(style.left.slice(0, -2));
+
+    const destinies = carousel.childNodes;
+    let direction;
+    let canMove;
+    
+    if(currentIndex > destinyIndex) {
+      direction = "backward";
+    }
+    if(currentIndex < destinyIndex) {
+      direction = "forward";
+    }
+
+    if(destinyIndex < destinies.length || destinyIndex >= 0) {
+      canMove = true;
+    }
+
+    if(direction === "forward" && canMove) {
+      setCurrentIndex(destinyIndex);
+      carousel.style.left = (currentDistance - distance) + "px";
+
+      destinies.forEach((element, index) => {
         element.classList.add(styles.unfocused);
-        if(carouselIndex === index) {
+        if(destinyIndex === index) {
           element.classList.remove(styles.unfocused);
         }
       });
-      return;
+    }
+
+    if(direction === "backward" && canMove){
+      setCurrentIndex(destinyIndex);
+      carousel.style.left = (currentDistance + distance) + "px";
+
+      destinies.forEach((element, index) => {
+        element.classList.add(styles.unfocused);
+        if(destinyIndex === index) {
+          element.classList.remove(styles.unfocused);
+        }
+      });
     }
   }
 
-  const debouncedMoveCarousel = useCallback(throttle(moveCarousel, 200), []);
+  const debouncedMoveCarousel = useCallback(throttle(calculateDistance, 200), []);
   
   return (
     <section id={styles.experience}>
@@ -140,16 +132,22 @@ function Experience() {
       </div>
       <div className={styles.jobDetails} id="jobDetails">
         <div className={styles.carouselWrapper}>
-          <button className={styles.carouselBtn} onClick={() => debouncedMoveCarousel('backward')}>&#8592;</button>
+          <button className={styles.carouselBtn} onClick={() => debouncedMoveCarousel(currentIndex - 1)}>&#8592;</button>
             <div className={styles.carouselFocus}>
               <div className={styles.carousel}>
-                  <Image className={`${styles.element} ${styles.landscape}`} src={'/carousel/desktopOne.jpg'} width={1000} height={1000} alt="test"></Image>
-                  <Image className={`${styles.element} ${styles.portrait} ${styles.unfocused}`} src={'/carousel/cellphoneOne.png'} width={1000} height={1000} alt="test"></Image>
-                  <Image className={`${styles.element} ${styles.landscape} ${styles.unfocused}`} src={'/carousel/desktopTwo.jpg'} width={1000} height={1000} alt="test"></Image>
-                  <Image className={`${styles.element} ${styles.portrait} ${styles.unfocused}`} src={'/carousel/cellphoneTwo.jpg'} width={1000} height={1000} alt="test"></Image>
+                  <Image className={`${styles.landscape}`} src={'/carousel/desktopOne.jpg'} width={1000} height={1000} alt="test"></Image>
+                  <Image className={`${styles.portrait} ${styles.unfocused}`} src={'/carousel/cellphoneOne.png'} width={1000} height={1000} alt="test"></Image>
+                  <Image className={`${styles.landscape} ${styles.unfocused}`} src={'/carousel/desktopTwo.jpg'} width={1000} height={1000} alt="test"></Image>
+                  <Image className={`${styles.portrait} ${styles.unfocused}`} src={'/carousel/cellphoneTwo.jpg'} width={1000} height={1000} alt="test"></Image>
               </div>
             </div> 
-          <button className={styles.carouselBtn} onClick={() => debouncedMoveCarousel('forward')}>&#8594;</button>
+          <button className={styles.carouselBtn} onClick={() => debouncedMoveCarousel(currentIndex + 1)}>&#8594;</button>
+        </div>
+        <div className={styles.radioWrapper}>
+          <div className={`${styles.radio} ${styles.selected}`} onClick={toggleRadio}></div>
+          <div className={`${styles.radio}`} onClick={toggleRadio}></div>
+          <div className={`${styles.radio}`} onClick={toggleRadio}></div>
+          <div className={`${styles.radio}`} onClick={toggleRadio}></div>
         </div>
       </div>
     </section>
